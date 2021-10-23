11f19110957c40866235140b4178b5dcc1a5288a
Banban <banbanus911@gmail.com>
2021-10-23 21:03:40 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 import AppLayout from "./components/layout/AppLayout";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <AppLayout />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 reportWebVitals();
